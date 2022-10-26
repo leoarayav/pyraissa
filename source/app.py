25a067dccfbdfd1901adbe787bfadbbcc8388a59
leoarayav <leandro.araya08@inacapmail.cl>
2022-10-26 17:27:21 -0300

    style (app): empty lines.

--- source/app.py
+++ source/app.py
@@ -2,12 +2,10 @@ from PyQt5 import QtCore, QtGui, QtWidgets
 from ui.main import Ui_MainWindow
 import sys
 
-
 class App(QtWidgets.QMainWindow, Ui_MainWindow):
     def __init__(self):
         super().__init__()
         self.setupUi(self)
-
 
 if __name__ == "__main__":
     import sys
